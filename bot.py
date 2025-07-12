327f6b8cd33edb15089effbd60ede514f8c098b1
Syed Afrid <afrid0052@gmail.com>
2025-07-13 00:05:52 +0530

    Add horoscope command handling and user interaction flow

--- bot.py
+++ bot.py
@@ -28,5 +28,27 @@ def get_daily_horoscope(sign: str, day: str) -> dict:
 
     return response.json()
 
+@bot.message_handler(commands=['horoscope'])
+def sign_handler(message):
+    text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
+    sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(sent_msg, day_handler)
+
+def day_handler(message):
+    sign = message.text
+    text = "What day do you want to know?\nChoose one: *TODAY*, *TOMORROW*, *YESTERDAY*, or a date in format YYYY-MM-DD."
+    sent_msg = bot.send_message(
+        message.chat.id, text, parse_mode="Markdown")
+    bot.register_next_step_handler(
+        sent_msg, fetch_horoscope, sign.capitalize())
+    
+def fetch_horoscope(message, sign):
+    day = message.text
+    horoscope = get_daily_horoscope(sign, day)
+    data = horoscope["data"]
+    horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
+    bot.send_message(message.chat.id, "Here's your horoscope!")
+    bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
 # To launch the bot
 bot.infinity_polling()
