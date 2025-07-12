171c9b83a6dd65525373037e876b67b22b09d79c
Syed Afrid <afrid0052@gmail.com>
2025-07-12 23:49:08 +0530

    fetch data

--- bot.py
+++ bot.py
@@ -13,5 +13,20 @@ def send_welcome(message):
 def echo_all(message):
     bot.reply_to(message, message)
 
+import requests
+
+def get_daily_horoscope(sign: str, day: str) -> dict:
+    """Get daily horoscope for a zodiac sign.
+    Keyword arguments:
+    sign:str - Zodiac sign
+    day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
+    Return:dict - JSON data
+    """
+    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
+    params = {"sign": sign, "day": day}
+    response = requests.get(url, params)
+
+    return response.json()
+
 # To launch the bot
 bot.infinity_polling()
