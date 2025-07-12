37eefb966bd2f8c1c2f714a884926aa7599669f7
Syed Afrid <afrid0052@gmail.com>
2025-07-13 00:07:47 +0530

    bot launch

--- bot.py
+++ bot.py
@@ -8,31 +8,12 @@ bot = telebot.TeleBot(BOT_TOKEN)
 def send_welcome(message):
     bot.reply_to(message, 'Hello and Welcome, I am your Horoscope bot!')
 
-# Echo back the message handler
-@bot.message_handler(func = lambda msg: True)
-def echo_all(message):
-    bot.reply_to(message, message)
-
-import requests
-
-def get_daily_horoscope(sign: str, day: str) -> dict:
-    """Get daily horoscope for a zodiac sign.
-    Keyword arguments:
-    sign:str - Zodiac sign
-    day:str - Date in format (YYYY-MM-DD) OR TODAY OR TOMORROW OR YESTERDAY
-    Return:dict - JSON data
-    """
-    url = "https://horoscope-app-api.vercel.app/api/v1/get-horoscope/daily"
-    params = {"sign": sign, "day": day}
-    response = requests.get(url, params)
-
-    return response.json()
-
 @bot.message_handler(commands=['horoscope'])
 def sign_handler(message):
     text = "What's your zodiac sign?\nChoose one: *Aries*, *Taurus*, *Gemini*, *Cancer,* *Leo*, *Virgo*, *Libra*, *Scorpio*, *Sagittarius*, *Capricorn*, *Aquarius*, and *Pisces*."
     sent_msg = bot.send_message(message.chat.id, text, parse_mode="Markdown")
     bot.register_next_step_handler(sent_msg, day_handler)
+
 
 def day_handler(message):
     sign = message.text
@@ -49,6 +30,12 @@ def fetch_horoscope(message, sign):
     horoscope_message = f'*Horoscope:* {data["horoscope_data"]}\\n*Sign:* {sign}\\n*Day:* {data["date"]}'
     bot.send_message(message.chat.id, "Here's your horoscope!")
     bot.send_message(message.chat.id, horoscope_message, parse_mode="Markdown")
+
+# Echo back the message handler
+@bot.message_handler(func = lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message)
+
 
 # To launch the bot
 bot.infinity_polling()
